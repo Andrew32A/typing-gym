37755896ee026b09dcad20760acc33817f340175
Andrew Alsing <111094607+Andrew32A@users.noreply.github.com>
2022-11-15 13:30:26 -0800

    minor changes and added key checker

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ const random_quote_api_url = 'https://api.quotable.io/random'
 const quote_display = document.getElementById("quoteDisplay")
 const quote_input = document.getElementById("quoteInput")
 
-function get_quote(){
+function get_quote() {
     return fetch(random_quote_api_url)
         .then(response => response.json())
         .then(data => data.content)
@@ -12,14 +12,22 @@ function get_quote(){
 async function get_next_quote(){
     const quote = await get_quote()
     quote_display.innerText = quote
+    quote.split("")
+    // quote_input.value = null
     console.log(quote)
 }
 
 get_next_quote()
 
-// check user input
+// get user input
+document.addEventListener("keydown", (e) => {
+    let name = e.key;
+    let code = e.code;
+
+    console.log(`key pressed: ${name}, key code: ${code}`);
+})
 
 
-// return "correct" or "incorrect" comparing user's input
+// check user input against api string | return "correct" or "incorrect" comparing user's input
 
 // add WPM counter, add a value that goes up by one evert time the correct key is pressed and divide the total by 60
