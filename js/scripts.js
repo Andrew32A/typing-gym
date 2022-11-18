072060376b61ff26a2d1be8306228af566198933
Andrew Alsing <111094607+Andrew32A@users.noreply.github.com>
2022-11-18 15:33:01 -0800

    added class assignments for correct and incorrect for spans

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,10 @@ const quote_display = document.getElementById("quoteDisplay")
 const quote_input = document.getElementById("quoteInput")
 
 // init variables
-let quoteSplit
+let quoteSplit // contains array for quote
+let counter = 0;
+let characterSpan = document.createElement("span")
+let characterSpanArray = []
 
 // fetches quote from api
 function getQuote() {
@@ -19,7 +22,8 @@ async function getNextQuote() {
     quote_display.innerText = ""
     quoteSplit = quote.split("")
     quoteSplit.forEach(character => {
-        const characterSpan = document.createElement("span")
+        characterSpan = document.createElement("span")
+        characterSpanArray.push(characterSpan)
         characterSpan.innerText = character
         quote_display.appendChild(characterSpan)
     });
@@ -27,35 +31,56 @@ async function getNextQuote() {
     console.log(quote)
 }
 
+
 // get user input
 document.addEventListener("keydown", (e) => {
-    let name = e.key;
+    let keyStroke = e.key;
     let code = e.code;
-    let counter = 0;
+    let character = quoteSplit[counter]
+    // let characters = quote_display.querySelectorAll("span")
 
     // change color to green for correct
-    if (name == getNextQuote[counter]) {
+    if (keyStroke === character) {
+        console.log(`key pressed: ${keyStroke}, key code: ${code}`);
+        console.log(counter)
+        console.log(character)
+        // let correctLetter = document.createElement("span")
+        // correctLetter.innerText = keyStroke
+        // quote_display.appendChild(correctLetter)
+        
+        // adds "correct" class to span in index with correct key press
+        // character.classList.add = ("correct")
+        characterSpanArray[counter].classList.add("correct");
         counter += 1
-        let correctLetter = document.createElement("span")
-        correctLetter.innerText = name
-        quote_display.appendChild(correctLetter)
     }
 
     // change color to red for incorrect
-    else if (name != getNextQuote[counter]) {
-        counter += 1
+    else if (keyStroke != quoteSplit[counter]) {
         let incorrectLetter = document.createElement("span")
-        incorrectLetter.innerText = name
+        incorrectLetter.innerText = keyStroke
         quote_display.appendChild(incorrectLetter)
+        
+        // adds "incorrect" class to span in index with incorrect key press
+        characterSpanArray[counter].classList.add("incorrect");    
+        counter += 1
     }
 
     // if return is pressed, then undo color change
-    else if (name == "Backspace") {
+    else if (keyStroke === "Backspace") {
         counter -= 1
+        
+        // removes "incorrect" and "correct" class from span in index when user inputs "Backspace"
+        characterSpanArray.classList.remove("correct");    
+        characterSpanArray.classList.remove("incorrect");   
     }
 
-    console.log(`key pressed: ${name}, key code: ${code}`);
+    console.log(`key pressed: ${keyStroke}, key code: ${code}`);
     console.log(counter)
+
+    // console.log(characters[0])
+    // console.log(quote_display[0])
+    // console.log(getNextQuote[0])
+    console.log(character)
 })
 
 
