5d49c2a57f43d09f8634f52a1c8a0a08fc853304
Andrew Alsing <111094607+Andrew32A@users.noreply.github.com>
2022-11-19 15:34:52 -0800

    minor changes

--- js/welcome.js
+++ js/welcome.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-	//Wrapper animation
+	//wrapper animation
 	anime.timeline({
 		 targets: ".welcome",
 		 easing: "easeOutExpo",
 	})
 	 .add({
-	 width: ["0vw", "100vw"],
-	 opacity: 1,
-	 duration: 1200,
+		width: ["0vw", "100vw"],
+		opacity: 1,
+		duration: 1200,
 	})
 	 .add({
-	 delay: 2700,
-	 translateX: "100vw",
-	 duration: 1500,
-	 complete: function(anime) {
+		delay: 2700,
+		translateX: "100vw",
+		duration: 1500,
+		complete: function(anime) {
 		 document.querySelector('.welcome').remove();
 	 }
 	})
 
-	//Text animation
+	//text animation
 	anime({
 		targets: ".heading",
 		delay: 400,
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		easing: "easeOutExpo",
 	});
 
-	//Text-sm animation
+	//text-sm animation
 	anime({
 		targets: ".sub-heading",
 		delay: 600,
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		easing: "easeOutExpo",
 	});
 
-	//Loader animation
+	//loader animation
 	anime({
 		targets: ".loader",
 		delay: 2000,
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		easing: "easeOutExpo",
 	});
 
-	//Loader-wrapper animation
+	//loader-wrapper animation
 	anime({
 		targets: ".loader-wrapper",
 		delay: 1500,
@@ -57,13 +57,13 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 })
 
-//Paragraphs
+//paragraphs
 	anime({
 		targets: 'p',
-  	opacity: 1,
-  	duration: 1800,
-  	translateY: ["-30px", "0px"],
-  	easing: "easeOutExpo",
+  		opacity: 1,
+  		duration: 1800,
+  		translateY: ["-30px", "0px"],
+  		easing: "easeOutExpo",
 		delay: (el, i) => 5200 + 100 * i,
 	})
 
