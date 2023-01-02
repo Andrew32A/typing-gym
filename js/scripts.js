7162276df8c6288b17dc6e69bf6b1d2ad94aece9
Andrew Alsing <111094607+Andrew32A@users.noreply.github.com>
2023-01-01 20:23:46 -0800

    merged code from https://github.com/Andrew32A/minimalist-pop-out-menu

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,9 @@ const resultsDisplay = document.getElementById("results")
 const accuracyDisplay = document.getElementById("accuracyDisplay")
 const charactersDisplay = document.getElementById("charactersDisplay")
 
+const menuParent = document.querySelector(".menuParent")
+const itemArray = document.querySelectorAll(".item") // item is an array when getting it by query selector all
+
 // init variables
 // const fs = require('fs')
 let quoteSplit // contains array for quote that i later reassign to character
@@ -18,11 +21,14 @@ let counter = 0 // stores keys hit per quote
 let totalCounter = 0 // stores total counter per session
 let mistakesCounter = 0
 let characterSpan = document.createElement("span")
-let timeLeft = 30
 let isTimerStarted = true
 let timerInterval // allows interval to be seen by other functions
 let allowTyping = true
 let offlineIndex // random quote from offline json index
+
+let timeTotal = 30
+let timeLeft = 30
+let menuCounter = 0
 
 // grabs quote from json incase the api is offline
 function offlineQuotes() {
@@ -290,3 +296,82 @@ document.addEventListener("keydown", (e) => {
 
 // starts loop
 getNextQuote()
+
+
+// helper function to calculate width based on num of items
+function widthCalculator() {
+    // takes length of array, adds 1 for menuParent, then times it by 73px which was from the original value (365px / 5)
+    let width = `${((itemArray.length + 1) * 73)}px`
+    return width
+}
+
+// opens menu
+function openMenu() {
+    anime({
+        targets: "#menu",
+        backgroundColor: "#151a21",
+        width: widthCalculator(),
+        easing: "easeInOutQuad"
+    })
+
+    for (let i = 0; i < itemArray.length; i++) {
+        anime ({
+            targets: itemArray[i],
+            translateX: (60 * (i + 1)),
+            opacity: 2,
+            easing: "easeInOutQuad"
+            // i really wanted to use stagger to handle each item element, but it's only available on the latest version of anime.js which i couldn't get to work right now
+            // instead, i used a for loop to handle each element
+            // delay: anime.stagger(100)
+        })
+
+        itemArray[i].style.visibility = "visible"
+    }
+}
+
+// closes menu
+function closeMenu() {
+    anime({
+        targets: "#menu",
+        backgroundColor: "#1b2028",
+        width: "125px",
+        easing: "easeOutQuint"
+    })
+
+    for (let i = 0; i < itemArray.length; i++) {
+        anime ({
+            targets: itemArray[i],
+            translateX: 0,
+            opacity: -2 // had to set opacity to -2 to avoid weird stutter from bounceback effect in anime.js
+        })
+
+        itemArray[i].style.visibility = "hidden"
+    }
+}
+
+// counts the num of times the user clicked on menu and handles menu behaviour as needed
+function menuToggle() {
+    menuCounter++
+    if (menuCounter % 2 === 0) {
+        closeMenu()
+    }
+    else {
+        openMenu()
+    }
+}
+
+// activates menu functionality, remove this to disable menu
+menuParent.addEventListener("click", menuToggle)
+
+// changes menu parent value depending on which item was clicked
+function menuChange(item) {
+    console.log(item)
+    for (i = 0; i < itemArray.length; i++) {
+        if (item.id === itemArray[i].id) {
+            menuParent.innerHTML = itemArray[i].innerHTML
+        }
+    }
+    
+    menuCounter++
+    closeMenu()
+}
